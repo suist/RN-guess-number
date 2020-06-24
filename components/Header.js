726c0aa07a0f17cc1e5ae1847d3ae16c8b8a7d80
su <skoco73@gmail.com>
2020-06-24 15:49:34 +0900

    Platform.OS / Platform.select(

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.header,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
@@ -14,10 +22,12 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 90,
     paddingTop: 36,
-    backgroundColor: "#b5eaff",
+    backgroundColor: Platform.OS === "android" ? "#b5eaff" : "white",
     alignItems: "center",
     justifyContent: "center",
   },
+  headerIOS: {},
+  headerAndroid: {},
   headerTitle: {
     color: "black",
     fontSize: 18,
